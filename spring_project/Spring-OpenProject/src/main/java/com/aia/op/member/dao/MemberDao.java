3e56d0c88ba8cd943f6bdafe11b4018a705a256e
Khyo0 <71997990+Khyo0@users.noreply.github.com>
2021-01-15 23:20:31 +0900

    2021.01.15

--- spring_project/Spring-OpenProject/src/main/java/com/aia/op/member/dao/MemberDao.java
+++ spring_project/Spring-OpenProject/src/main/java/com/aia/op/member/dao/MemberDao.java
@@ -15,17 +15,23 @@ public interface MemberDao {
 	// 로그인
 	Member selectLogin(String id, String pw);
 
-	// 회원의 총 수
+	// 총 회원 수
 	int selectTotalCount();
 
 	// 회원 리스트(페이지 번호에 맞는)
 	//List<Member> selectMemberList(int startRow, int cntPerPage);
 	List<Member> selectMemberList(Map<String , Object> param);
 
-	// 검색한 회원의 수
+	// 검색 회원 수
 	int selectSearchMemberCount(Map<String, Object> listMap);
 
 	// 회원 정보 삭제
 	int deleteMemberByIdx(int idx);
+
+	// 회원 정보 조회 : idx로 조회 
+	Member selectMemberByIdx(int idx);
+
+	// 회원 정보 update
+	int updateMember(Member member);
 	
 }
